15ba479efbb17daa70256d996fc09b5ebfe07113
manosp24 <72653039+manosp24@users.noreply.github.com>
2023-08-11 07:45:20 +0000

    NznzjansmznK.

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -5,5 +5,5 @@ export default function ComingSoon() {
     <div style={{color: "white"}}>
       <h1 style = {{fontFamily: "'Tektur', cursive", fontSize: 50, color: "rgb(255, 10, 10)"}}>COMING SOON.</h1>
     </div>
-  )
+    )
 }
